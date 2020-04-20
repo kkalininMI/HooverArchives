f24a31888ee4223fdc88ede234010cc66479b3e7
Kirill Kalinin <kkalinin@umich.edu>
2020-04-20 13:21:07 -0700

    removed ";" at the end of the line

--- R/dateReformatter.R
+++ R/dateReformatter.R
@@ -112,7 +112,7 @@ dateReformatter<-function(datV){
       vector.s.year<-gsub("\\s+-\\s+", "-", vector.s.year)
       vector.s.year<-gsub("[[:punct:][:blank:]]+$", "", vector.s.year, perl=TRUE)
       vector_combined<-paste(paste(vector.year, vector.s.year, sep=":"), collapse=" ")
-      vector_combined<-gsub("(,\\s*$)|((?<=.)$)|(\\s*$)", ";", vector_combined, perl=TRUE)
+      vector_combined<-gsub("(,\\s*$)|((?<=.)$)|(\\s*$)", "", vector_combined, perl=TRUE)
       vector_combined<-gsub("(\\s(?=\\d{4}:))", "; ", vector_combined, perl=TRUE)
       vector_combined<-gsub("(:;)|(,;)", ";", vector_combined, perl=TRUE)
       vector_combined<-gsub("(^:)|(\n)", "", vector_combined, perl=TRUE)
